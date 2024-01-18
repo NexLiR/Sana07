99f3a9917669568c005dc90aa84d2e1ec437da72
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2024-01-18 18:04:27 +0200

    Add ShoppingCart class

--- Program/ShoppingCart.cs
+++ Program/ShoppingCart.cs
@@ -4,6 +4,30 @@ namespace Sana07
 {
     public class ShoppingCart
     {
-
+        protected List<Product> products;
+        public void addToShoppingCart(Product product)
+        {
+            products.Add(product);
+        }
+        public void removeFromShoppingCart(Product product)
+        {
+            products.Remove(product);
+        }
+        public void showShoppingCart()
+        {
+            foreach (Product product in products)
+            {
+                product.Display();
+            }
+        }
+        public float calcilateTotalPrice()
+        {
+            float totalPrice = 0;
+            foreach (Product product in products)
+            {
+                totalPrice += product.Price;
+            }
+            return totalPrice;
+        }
     }
 }

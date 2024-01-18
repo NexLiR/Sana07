c28afe8b36fa3da6e762a5db67f39d07f40bb24a
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2024-01-18 16:48:02 +0200

    Add abstract class Product

---
+++ Program/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Program
+{
+    public abstract class Product
+    {
+        protected String _name;
+        protected float _price;
+        protected int _count;
+
+        public String Name
+        {
+            get { return _name; }
+            set 
+            {
+                if (value.Length > 0) _name = value;
+                else throw new ArgumentException("Name of product can not be empty!");
+            }
+        }
+        public float Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0) _price = value;
+                else throw new ArgumentException("Price of product can not be negative!");
+            }
+        }
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                if (value >= 0) _count = value;
+                else throw new ArgumentException("Count of product can not be negative!");
+            }
+        }
+
+        public Product()
+        {
+            Name = "Undefined";
+            Price = 0f;
+            Count = 0;
+        }
+        public Product(String name, float price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+        public Product(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+            Count = product.Count;
+        }
+
+        public abstract String Display();
+    }
+}
---
+++ Program/Smartphone.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Program
+{
+    public class Smartphone : Product
+    {
+
+    }
+}
